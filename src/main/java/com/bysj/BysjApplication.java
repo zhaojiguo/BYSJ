09a8dd49af01485055a78414ceadfcab0aaa0c20
z937925737 <zhaojiguo97@163.com>
2020-01-06 15:26:36 +0800

    集成beetle

--- src/main/java/com/bysj/BysjApplication.java
+++ src/main/java/com/bysj/BysjApplication.java
@@ -1,12 +1,15 @@
-package com.bysj.bysj;
+package com.bysj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bysj.dao")
 public class BysjApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BysjApplication.class, args);
     }
 
--- src/test/java/com/bysj/bysj/BysjApplicationTests.java
+++ src/test/java/com/bysj/bysj/BysjApplicationTests.java
@@ -1,13 +1,19 @@
 package com.bysj.bysj;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BysjApplicationTests {
 
+
     @Test
     void contextLoads() {
+
     }
 
 }
